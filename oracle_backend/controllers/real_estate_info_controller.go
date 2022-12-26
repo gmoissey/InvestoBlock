06efe3aa19fa61c3d29cccca29de81ff456dc0db
gmoissey <gmoisseyev2@huskers.unl.edu>
2022-12-25 20:34:57 -0600

    added put/delete endoints to complete CRUD functionality

--- oracle_backend/controllers/real_estate_info_controller.go
+++ oracle_backend/controllers/real_estate_info_controller.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -58,7 +59,7 @@ func GetRealEstateByID() gin.HandlerFunc {
 		id, _ := primitive.ObjectIDFromHex(idString)
 
 		//find the real estate by id
-		err := realEstateInfoCollection.FindOne(context, models.RealEstate{ID: id}).Decode(&realEstate)
+		err := realEstateInfoCollection.FindOne(context, bson.M{"_id": id}).Decode(&realEstate)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -68,6 +69,79 @@ func GetRealEstateByID() gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Real estate found successfully",
 			"result":  realEstate,
+		})
+	}
+}
+
+func UpdateRealEstateById() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		//validate the request body
+		var realEstate models.RealEstate
+		if err := ctx.ShouldBindJSON(&realEstate); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		//use the validator library to validate required fields
+		if err := validate.Struct(realEstate); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		//get the real estate id
+		idString := ctx.Param("id")
+		id, _ := primitive.ObjectIDFromHex(idString)
+
+		update := bson.M{
+			"market_price": realEstate.MarketPrice,
+			"address":      realEstate.Address,
+			"city":         realEstate.City,
+			"state":        realEstate.State,
+			"zip_code":     realEstate.ZipCode,
+			"beds":         realEstate.Beds,
+			"baths":        realEstate.Baths,
+			"sqft":         realEstate.Sqft,
+			"year_built":   realEstate.YearBuilt,
+		}
+
+		//update the real estate
+		result, err := realEstateInfoCollection.UpdateOne(context, bson.M{"_id": id}, bson.M{"$set": update})
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		//return the updated real estate
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Real estate updated successfully",
+			"result":  result,
+		})
+	}
+}
+
+func DeleteRealEstateById() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		//get the real estate id
+		idString := ctx.Param("id")
+		id, _ := primitive.ObjectIDFromHex(idString)
+
+		//delete the real estate
+		result, err := realEstateInfoCollection.DeleteOne(context, bson.M{"_id": id}) //delete the real estate
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		//return the deleted real estate
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Real estate deleted successfully",
+			"result":  result,
 		})
 	}
 }
--- oracle_backend/routes/real_estate_info_routes.go
+++ oracle_backend/routes/real_estate_info_routes.go
@@ -9,4 +9,6 @@ import (
 func RealEstateInfoRoutes(router *gin.Engine) {
 	router.GET("/real_estate_info/:id", controllers.GetRealEstateByID())
 	router.POST("/real_estate_info", controllers.CreateRealEstate())
+	router.PUT("/real_estate_info/:id", controllers.UpdateRealEstateById())
+	router.DELETE("/real_estate_info/:id", controllers.DeleteRealEstateById())
 }
